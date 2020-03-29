ca0d9530a7005df581bcd45e5ed9b0b240238c37
tuuchen <tuuchen@outlook.com>
2020-03-30 00:55:40 +0300

    Update

--- vue-guestbook-app/server.js
+++ vue-guestbook-app/server.js
@@ -1,55 +1,54 @@
 const express = require('express');
+const cors = require('cors');
 const serveStatic = require('serve-static');
 const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const app = express();
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(cors());
+app.use(bodyParser.json());
 
-// Path for serving static files
-app.use('/', serveStatic(path.join(__dirname, '/src')));
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
 
-// Guestbook -page
-app.get('/guestbook', (req, res) => {
+// Load json and send to frontend
+app.get('/api/guestbook/', (req, res) => {
     let data = fs.readFileSync('./guestbook.json');
     data = JSON.parse(data);
-    res.render('pages/guestbook', { data });
-});
-
-// Add entry -page
-app.get('/newmessage', (req, res) => {
-    data = [{ id: '', username: '', country: '', date: '', message: '' }]
-    res.render('pages/newmessage', { data });
+    res.send(data);
 });
 
 // Add form data to json and return new data
 app.post('/api/newmessage/', (req, res) => {
-    let guestbook = fs.readFileSync('./guestbook.json');
-    guestbook = JSON.parse(guestbook);
+    let data = fs.readFileSync('./guestbook.json');
+    data = JSON.parse(data);
     let form = req.body
-    guestbook.push(form)
-    data = []
     data.push(form)
-    fs.writeFileSync('./guestbook.json', JSON.stringify(guestbook));
-    res.render('pages/newmessage', { data });
+    fs.writeFileSync('./guestbook.json', JSON.stringify(data));
+    data = fs.readFileSync('./guestbook.json');
+    res.send(data)
 });
 
-// Delete data from json by ID
+// Send submitted form back to frontend
+app.post('/api/ajax/', (req, res) => {
+    let data = req.body
+    res.send(data)
+});
+
+// Delete data from json by ID and return new data
 app.delete('/api/delete/:id', (req, res) => {
     let id = req.params.id
     let data = fs.readFileSync('./guestbook.json');
     let json = JSON.parse(data);
     json = json.filter((json) => { return json.id !== id });
     fs.writeFileSync('./guestbook.json', JSON.stringify(json, null, 2));
-    res.end();
+    data = fs.readFileSync('./guestbook.json');
+    res.send(data)
 });
 
-// Redirect unkown routes to index
+// this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
-    res.render('pages/index');
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
 const port = process.env.PORT || 8081;
